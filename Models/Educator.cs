102e744dcd688129d6ef226b52c8bb3b7964a148
Paweł Łęczycki <pawel@msd.info.pl>
2020-06-22 16:14:19 +0200

    Educator

---
+++ Models/Educator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{ 
+    public class Educator : ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string FullName => $"{LastName} {FirstName}";
+
+        public Specialization Specialization { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Models/Specialization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public enum Specialization
+    {
+        Math,
+        Physics,
+        Chemistry,
+        Biology,
+        Sociology,
+        Psychology
+    }
+}
