aadaa6efb81a4a3a6c4521c026aed5d1c611e499
Paweł Łęczycki <pawel@msd.info.pl>
2020-06-22 13:55:59 +0200

    Komenda Delete

--- WpfApp/ViewModel/StudentsViewModel.cs
+++ WpfApp/ViewModel/StudentsViewModel.cs
@@ -1,14 +1,25 @@
 ﻿using Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp.Commands;
 
 namespace WpfApp.ViewModel
 {
     public class StudentsViewModel
     {
-        public ICollection<Student> Students { get; set; } = new List<Student> { new Student { FirstName = "Adam", LastName = "Adamski" }, new Student { FirstName = "Ewa", LastName = "Ewowska" } };
+        public StudentsViewModel()
+        {
+            DeleteCommand = new CustomCommand(x => Students.Remove(SelectedStudent), x => Students.Contains(SelectedStudent));
+        }
+
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student> { new Student { FirstName = "Adam", LastName = "Adamski" }, new Student { FirstName = "Ewa", LastName = "Ewowska" } };
+        public Student SelectedStudent { get; set; }
+
+        public ICommand DeleteCommand { get; }
     }
 }
