17ed2cf005305b1bfbc5a77dd2f5b4df0cd75591
Paweł Łęczycki <pawel@msd.info.pl>
2020-06-22 11:57:11 +0200

    Rozbudowa okna studentów w aplikacji WPF

---
+++ WpfApp/ViewModel/StudentsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.ViewModel
+{
+    public class StudentsViewModel
+    {
+        public ICollection<Student> Students { get; set; } = new List<Student> { new Student { FirstName = "Adam", LastName = "Adamski" }, new Student { FirstName = "Ewa", LastName = "Ewowska" } };
+    }
+}
